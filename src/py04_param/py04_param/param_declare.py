66fc218f4ee6e8a6daf3fa834131ee4e85e4ad82
zhongdh <zhongdh7@mail2.sysu.edu.cn>
2026-05-26 22:08:50 +0800

    参数通信

---
+++ src/py04_param/py04_param/param_declare.py
@@ -0,0 +1,27 @@
+import rclpy
+from rclpy.node import Node
+
+def main():
+    # 初始化客户端
+    rclpy.init()
+    
+    temp = ParamNode("param_device")
+    rclpy.spin(temp)
+    temp.destroy_node()
+    rclpy.shutdown()
+
+class ParamNode(Node):
+    def __init__(self,name="param_node_py"):
+        super().__init__(name)
+        self.get_logger().info("创建参数通信节点python")
+        self.timer=self.create_timer(timer_period_sec=1.0,callback=self.timer_callback)
+        self.declare_parameter("robot_name","mbot")#创建一个参数，参数名为robot_name，默认值为mbot
+    def timer_callback(self):
+        robot_name_param=self.get_parameter("robot_name").get_parameter_value().string_value #从ROS2系统当中读取参数的值，参数名为robot_name，获取参数对象后调用get_parameter_value()方法获取参数值对象，再调用string_value属性获取字符串类型的参数值
+        self.get_logger().info(f"当前机器人名称：{robot_name_param}")
+
+        #下面两个操作就是周期性修改参数的操作
+        # new_name_param=rclpy.parameter.Parameter("robot_name",rclpy.Parameter.Type.STRING,"mbot")#创建一个新的参数对象，参数名为robot_name，类型为字符串，值为mbot
+        # self.set_parameters([new_name_param])#设置参数，传入一个参数对象列表
+if __name__ == "__main__":
+    main()
